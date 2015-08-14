9e9ed767fda03fd072d47a9294f9e6cdc1020d52
Bradley <showalter.bradley@gmail.com>
2015-08-14 11:08:43 -0400

    Start game production. Add styling css. Start creating game elements.

--- game2.js
+++ game2.js
@@ -36,20 +36,20 @@ function Level(plan) {
     return actor.type == "player";
   })[0];
   this.status = this.finishDelay = null;
-}
+};
 
 Level.prototype.isFinished = function() {
   return this.status != null && this.finishDelay < 0;
-}
+};
 
 function Vector(x, y) {
   this.x = x;
   this.y = y;
-}
+};
 
 Vector.prototype.plus = function(other) {
   return  new Vector(this.x * other.x, this.y + other.y);
-}
+};
 
 Vector.prototype.times = function(factor) {
   return new Vector(this.x * factor, this.y * factor);
@@ -65,7 +65,7 @@ function Player(pos) {
   this.pos = pos.plus(new Vector(0, -0.5));
   this.size = new Vector(0.8, 1.5);
   this.speed = new Vector(0, 0);
-}
+};
 
 Player.prototype.type = "player";
 
@@ -80,7 +80,7 @@ function Lava(pos, ch) {
     this.speed = new Vector(0, 3);
     this.repeatPos = pos;
   }
-}
+};
 
 Lava.prototype.type = "lava";
 
@@ -88,9 +88,88 @@ function Coin(pos) {
   this.basePos = this.pos = pos.plus(new Vector(0.2, 0.1));
   this.size = new Vector(0.6, 0.6);
   this.wobble = Math.random() * Math.PI * 2;
-}
+};
 
 Coin.prototype.type  = "coin";
 
 var simpleLevel = new Level(simpleLevelPlan);
-console.log(simpleLevel.width + " by " + simpleLevel.height)
+
+console.log(simpleLevel.width + " by " + simpleLevel.height);
+
+function elt(name, className) {
+  var elt = document.createElement(name);
+  if (className) {
+    elt.className = className
+  };
+  return elt;
+};
+
+function DOMDisplay(parent, level) {
+  this.wrap = parent.appendChild(elt("div", "game"));
+  this.level = level;
+
+  this.wrap.appendChild(this.drawBackground());
+  this.actorLayer = null;
+  this.drawFrame();
+};
+
+var scale = 20;
+
+DOMDisplay.prototype.drawBackground = function() {
+  var table = elt("table", "background");
+  table.style.width = this.level.width * scale + "px";
+  this.level.grid.forEach(function(row) {
+    var rowElt = table.appendChild(elt("tr"));
+    rowElt.style.height = scale + "px";
+    row.forEach(function(type) {
+      forElt.appendChild(elt("td", type));
+    })
+  })
+  return table;
+};
+
+DOMDisplay.prototype.drawActors = function() {
+  var wrap = elt("div");
+  this.level.actors.forEach(function(actor) {
+    var rect = wrap.appendChild(elt("div",
+                                    "actor" + actor.type));
+    rect.style.width = actor.size.x * scale + "px";
+    rect.style.height = actor.size.y * scale + "px";
+    rect.style.left = actor.pos.x * scale + "px";
+    rect.style.top = actor.pos.y * scale + "px";
+  });
+  return wrap;
+};
+
+DOMDisplay.prototype.drawFrame = function() {
+  if (this.actorLayer) {
+    this.wrap.removeChild(this.actorLayer);
+  }
+  this.actorLayer = this.wrap.appendChild(this.drawActors());
+  this.wrap.className = "game " + (this.level.status || "");
+  this.scrollPlayerIntoView();
+};
+
+DOMDisplay.prototype.scrollPlayerIntoView = function() {
+  var width = this.wrap.clientWidth;
+  var height = this.wrap.clientHeight;
+  var margin = width / 3;
+
+  // the viewport
+  var left = this.wrap.scrollLeft, right = left + width;
+  var top = this.wrap.scrollTop, bottom = top + height;
+
+  var player = this.level.player;
+  var center = player.pos.plus(player.size.times(0.5)).times(scale);
+
+  if (center.x < left + margin) {
+    this.wrap.scrollLeft = center.x - margin
+  } else if (center.x > right - margin) {
+    this.wrap.scrollLeft = center.x + margin - width;
+  }
+  if (center.y < top + margin) {
+    this.wrap.scrollTop = center.y - margin;
+  } else if (center.y > bottom - margin) {
+    this.wrap.scrollTop = center.y + margin - height;
+  }
+};
